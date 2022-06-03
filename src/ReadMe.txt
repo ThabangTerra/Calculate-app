7de30b8ce26d4c7314d49b5c4484d4c06e83ea20
thabang <thabnagznkosi@gmail.com>
2022-06-03 13:56:03 +0200

    calculate

---
+++ src/ReadMe.txt
@@ -0,0 +1,16 @@
+5b8d0fd276b6d288905ed2f63a934e057e8feca2
+
+This is a simple angular 12 calculator Application.
+
+To access the project:
+
+NB: please make  sure you have node and angular on your machine before
+
+links: https://nodejs.org/en/ for node
+
+if you dont have angular: https://angular.io/cli
+1:Unzip the file 
+2:Extrat all the files 
+3:Open up your IDE 
+4: CD into the project eg "cd documents/calculate-app" 
+5:run ng serve
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
